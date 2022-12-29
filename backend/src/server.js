82d5010b58e0f0d63fd5ed7bb3941052554d67be
Str367 <str3670404@gmail.com>
2022-12-29 10:17:32 +0800

    hope

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(bodyParser.json()); //https://ithelp.ithome.com.tw/articles/10241083
 
 app.use("/api", routes);
-app.get("/api", (req, res) => {
-  res.send("Hello, World!");
-});
+// app.get("/api", (req, res) => {
+//   res.send("Hello, World!");
+// });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
